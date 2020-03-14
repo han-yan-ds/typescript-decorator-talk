91f482f947c8f6ac1caac147fa63bbdcb00021ee
Han Yan <hanprogramming@gmail.com>
2020-03-13 19:59:46 -0600

    Setup done

--- index.ts
+++ index.ts
@@ -1,30 +1,14 @@
 class Greeter {
-
-  @addName
   greet() {
     return 'Hello';
   }
 }
-
-// function addName(name) {
-
-function addName(constructor, methodName, methodDesc) {
-  const originalMethod = methodDesc.value;
-  const newMethodDesc = {
-    configurable: methodDesc.configurable,
-    enumerable: methodDesc.enumerable,
-    value: () => `${originalMethod()} ${name}`
-  };
-  return newMethodDesc;
-}
-// }
 
 
 function buttonPress() {
   const greet = new Greeter();
   return greet.greet();
 }
-
 
 
 document.getElementById('create-greeter-button')!.addEventListener("click", (e) => {
