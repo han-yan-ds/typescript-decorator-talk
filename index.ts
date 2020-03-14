2537c775415f696bbbb71ba022be5c999d39d297
Han Yan <hanprogramming@gmail.com>
2020-03-13 21:32:03 -0600

    Solution for multiple decorators

--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
 class Greeter {
+  @AddIAm('Han')
   @AddNameCustom('Josh') // "Decorating a method"
   greet() {
     return 'Hello';
+  }
+}
+
+
+function AddIAm(myName) {
+  /**
+   * Exercise:  Add another decorator that appends "I am myName" to the end
+   * Eg: "Hello Josh I am Andy"
+   * Hint: This will be a decorator factory (refer to branch 2DecoratorFactory)
+   */
+  return function(constructor, methodName, methodDescriptor) {
+    const originalMethod = methodDescriptor.value;
+    const newMethodDescriptor = {
+      configurable: methodDescriptor.configurable,
+      enumerable: methodDescriptor.enumerable,
+      value: () => `${originalMethod()} I am ${myName}`
+    };
+    return newMethodDescriptor;
   }
 }
 
